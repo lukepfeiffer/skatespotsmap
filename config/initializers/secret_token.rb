bee50698f2196f302c82f42e90432f174434fc99
Hashrocket Workstation <dev@hashrocket.com>
2014-07-07 22:06:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skatespotsmap::Application.config.secret_key_base = 'e010f4776c4107a8b5e84ddda73d6ded982ace83cc7f86f118d9fce9ae9dea6131c591bd8c7d94b4afe10a3f4d4a29a70381f0086a74ecd0004ccbfac578ea52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skatespotsmap::Application.config.session_store :cookie_store, key: '_skatespotsmap_session'
