83973dac2880d8f735fbfc3db87bb43d774bf140
Hashrocket Workstation <dev@hashrocket.com>
2014-07-27 13:04:33 -0400

    Fix styles on img

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     if params[:search].present?
       @spots = Spot.fuzzy_search(params[:search]).paginate(page: params[:page], per_page: 10).order(:city, :address_1)
     else
-      @spots = Spot.paginate(page: params[:page], per_page:2).order(:city, :address_1)
+      @spots = Spot.paginate(page: params[:page], per_page: 10).order(:city, :address_1)
     end
   end
 
