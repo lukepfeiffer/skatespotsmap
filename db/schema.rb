388fb90af3cfe165ccf3eaf5d230619215c41437
Hashrocket Workstation <dev@hashrocket.com>
2014-07-31 18:04:36 -0400

    Add trigram migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725022104) do
+ActiveRecord::Schema.define(version: 20140731215957) do
 
   create_table "spots", force: true do |t|
     t.text     "additional_info"
