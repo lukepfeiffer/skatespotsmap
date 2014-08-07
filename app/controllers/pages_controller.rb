56bc9a16a158aef671b996cf2e16e25b2c8dce47
Hashrocket Workstation <dev@hashrocket.com>
2014-08-06 22:30:12 -0400

    Comments belong to spots ;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     if params[:search].present?
-      @spots = Spot.fuzzy_search(params[:search]).paginate(page: params[:page], per_page: 15).order(:city, :address_1)
+      @spots = Spot.fuzzy_search(params[:search]).paginate(page: params[:page], order: 'created_at DESC', per_page: 15).order(:city, :address_1)
     else
-      @spots = Spot.paginate(page: params[:page], per_page: 15)
+      @spots = Spot.paginate(page: params[:page], order: 'created_at DESC',  per_page: 15)
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in(@user)
-      redirect_to new_spot_path
+      redirect_to root_path
     else
       redirect_to new_user_path
     end
